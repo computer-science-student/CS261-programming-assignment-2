e25221a20dff026e884a95524e9f1fbb0dbb4415
computer-science-student <legorrej@oregonstate.edu>
2021-07-16 23:03:12 -0500

    I changed the name of the parameters to make the BST be named tree. I started implementing the createBST() function.

--- bst_Fara_Le_Legorreta.c
+++ bst_Fara_Le_Legorreta.c
@@ -7,32 +7,39 @@
 
 // Creates the BST. Returns a pointer to the BST.
 struct BST* createBST(){
-    // TODO implement this function
-    return 0;
+    // Create the first node, root.
+    struct Node * root_prt = malloc(sizeof(struct Node*)); // free this malloc!
+    root_prt->left = NULL;
+    root_prt->right = NULL;
+
+    // Create the BST, tree.
+    struct BST * tree = malloc(sizeof(struct BST*)); // free this malloc!
+    tree->root = root_prt;
+    return tree;
 }
 
+void inorderTraversal(struct BST* tree){
+    // root head, top, tree,
 
-void insert(struct BST* node, int index){
+
+}
+
+void insert(struct BST* tree, int key){
     return NULL;
 }
 
 
-void inorderTraversal(struct BST* root){
+int height(struct BST* tree){
 
 }
 
-
-int height(struct BST* root){
-
-}
-
-int depth(struct BST* root, int key){
+int depth(struct BST* tree, int key){
 
 
 }
 
 
-void deleteBST(struct BST* root){
+void deleteBST(struct BST* tree){
 
 
 }
