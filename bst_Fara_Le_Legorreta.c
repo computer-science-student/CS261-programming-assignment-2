f0ecac7a4368ec7deb9372bc70be4ed06aeeebda
computer-science-student <legorrej@oregonstate.edu>
2021-07-16 21:44:36 -0500

    I tried to create a createBST function.

--- bst_Fara_Le_Legorreta.c
+++ bst_Fara_Le_Legorreta.c
@@ -5,3 +5,8 @@
 // Undefined reference to createBST() means that you have to create the function
 // for that here right?
 
+// Creates the BST. Returns a pointer to the BST.
+struct BST * createBST(){
+
+}
+
