c4ca36b072f0306e5da53d8955a32679bb0d4298
computer-science-student <legorrej@oregonstate.edu>
2021-07-16 22:40:19 -0500

    I filled in a bunch of the functions. They are empty right now and I am not sure if the parameters have good names.

--- bst_Fara_Le_Legorreta.c
+++ bst_Fara_Le_Legorreta.c
@@ -6,7 +6,33 @@
 // for that here right?
 
 // Creates the BST. Returns a pointer to the BST.
-struct BST * createBST(){
+struct BST* createBST(){
+    // TODO implement this function
+    return 0;
+}
+
+
+void insert(struct BST* node, int index){
+    return NULL;
+}
+
+
+void inorderTraversal(struct BST* root){
 
 }
 
+
+int height(struct BST* root){
+
+}
+
+int depth(struct BST* root, int key){
+
+
+}
+
+
+void deleteBST(struct BST* root){
+
+
+}
